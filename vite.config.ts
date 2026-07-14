8cc1193ba95b62cd2086517f01175b8eaa02cc2b
Diganta Bag <digantabag@gmail.com>
2026-07-15 01:22:57 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/secure-web-messenger/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
